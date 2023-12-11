6b63ed0b1572ef3b38c204edcd80b65cfe71f93a
Sk Akibuzzaman Jony <108605211+Sk-Akibuzzaman-Jony@users.noreply.github.com>
2023-12-11 21:42:21 +0530

    Create largest-subarray-with-0-sum.cpp

---
+++ Tries_and_maps/largest-subarray-with-0-sum.cpp
@@ -0,0 +1,24 @@
+// https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        map<int, int> mp;
+        int sum = 0;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            sum += A[i];
+            if(sum == 0){
+                ans = max(ans, i+1);
+            }
+            else if(mp.find(sum) == mp.end()){
+                mp[sum] = i;
+            }
+            else {
+                ans = max(ans, i - mp[sum]);
+            }
+        }
+        return ans;
+    }
+};
