c5ce280c8eaf7a81faded215c5f50f5627e223d6
Sk Akibuzzaman Jony <108605211+Sk-Akibuzzaman-Jony@users.noreply.github.com>
2023-12-29 23:36:36 +0530

    Create shortest-path-in-an-unweighted-graph-using-BFS.cpp

---
+++ Graph/shortest-path-in-an-unweighted-graph-using-BFS.cpp
@@ -0,0 +1,38 @@
+vector<int> shortestPath( vector<pair<int,int>> edges , int n , int m, int s , int t){
+	
+	//making a adjacecy matrix
+	vector<vector<int>> adj(n+1);
+	for(auto x : edges){
+		int parent = x.first;
+		int nbr = x.second;
+		adj[parent].push_back(nbr);
+		adj[nbr].push_back(parent);
+	}
+	vector<int> visited(n+1, false);
+	vector<int> parent(n+1);
+	queue<int> q;
+	q.push(s);
+	visited[s] = true;
+	parent[s] = -1;
+
+	while(!q.empty()){
+		int firstNode = q.front(); q.pop();
+		for(auto nbr : adj[firstNode]){
+			if(!visited[nbr]){
+				visited[nbr] = true;
+				parent[nbr] = firstNode;
+				q.push(nbr);
+			}
+		}
+	}
+
+	vector<int> ans;
+	while(t != -1){
+		ans.push_back(t);
+		t = parent[t];
+	}
+
+	reverse(ans.begin(), ans.end());
+	return ans;
+		
+}
