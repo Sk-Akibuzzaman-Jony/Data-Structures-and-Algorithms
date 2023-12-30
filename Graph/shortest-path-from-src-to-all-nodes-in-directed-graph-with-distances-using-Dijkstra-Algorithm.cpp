66bf7601617a6f0f8ccb9d38f75507139f7efbf0
Sk Akibuzzaman Jony <108605211+Sk-Akibuzzaman-Jony@users.noreply.github.com>
2023-12-30 12:01:16 +0530

    Create shortest-path-from-src-to-all-nodes-in-directed-graph-with-distances-using-Dijkstra-Algorithm.cpp

---
+++ Graph/shortest-path-from-src-to-all-nodes-in-directed-graph-with-distances-using-Dijkstra-Algorithm.cpp
@@ -0,0 +1,50 @@
+// https://practice.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1
+
+class Solution {
+  public:
+    // Dijkstra Algorithm
+     vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
+        //making an adj list 
+        vector<vector<vector<int>>> adj(N);
+        for(auto x : edges){
+            adj[x[0]].push_back({x[1], x[2]});
+        }
+        
+        //making a distance array
+        vector<int> dist(N, INT_MAX);
+        
+        //making a set of pair<distance, node>
+        set<pair<int, int>> s;
+        
+        dist[0] = 0;
+        s.insert({0, 0}); 
+        
+        while(!s.empty()){
+            //fetch the smallest or the first element of the set
+            auto firstNode = *s.begin();
+            s.erase(firstNode);
+            int node = firstNode.second;
+            int nodeDistance = firstNode.first;
+            
+            for(auto nbr : adj[node]){
+                if(nodeDistance + nbr[1] < dist[nbr[0]]){
+                    //update distance
+                    auto res = s.find({nbr[1], nbr[0]});
+                    if(res != s.end()){
+                        s.erase(res);
+                    }
+                    dist[nbr[0]] = nodeDistance + nbr[1];
+                    s.insert({dist[nbr[0]], nbr[0]});
+                }
+            }
+        }
+        
+        for(auto& x : dist){
+            if(x == INT_MAX){
+                x = -1;
+            }
+        }
+        
+        return dist;
+    }
+};
